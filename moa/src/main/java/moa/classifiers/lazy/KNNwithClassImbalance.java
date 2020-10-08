61fe70f134da0f581fc52c643ce7f6785d453447
Enrico <enrico.voltan@mail.polimi.it>
2020-10-08 19:35:09 +0200

    Knn with purity - not completed

--- moa/src/main/java/moa/classifiers/lazy/KNNwithClassImbalance.java
+++ moa/src/main/java/moa/classifiers/lazy/KNNwithClassImbalance.java
@@ -1,18 +1,27 @@
 package moa.classifiers.lazy;
 
+import com.github.javacliparser.FlagOption;
+import com.github.javacliparser.FloatOption;
+import com.github.javacliparser.IntOption;
 import com.yahoo.labs.samoa.instances.Instance;
 import com.yahoo.labs.samoa.instances.Instances;
 import com.yahoo.labs.samoa.instances.InstancesHeader;
 import moa.classifiers.lazy.neighboursearch.KDTree;
 import moa.classifiers.lazy.neighboursearch.LinearNNSearch;
 import moa.classifiers.lazy.neighboursearch.NearestNeighbourSearch;
+import org.apache.commons.math3.optim.SimplePointChecker;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class KNNwithClassImbalance extends kNN {
 
     protected ArrayList<Integer> sizes;
-    InstancesHeader context;
+
+    public FloatOption purifyFreq = new FloatOption( "purify", 'p', "purify the window with this frequence", 0.2, 0, 1);
+    public IntOption Kpurity = new IntOption( "KNNforpurity", 'v', "minimum number of neighbours of minority to purify", 3, 1, Integer.MAX_VALUE);
+    protected Random r;
 
 
     @Override
@@ -28,6 +37,7 @@ public class KNNwithClassImbalance extends kNN {
     @Override
     public void setModelContext(InstancesHeader context) {
         super.setModelContext(context);
+        r = new Random();
 
         this.sizes = new ArrayList<>();
         //this.window = new Instances(context,0); //new StringReader(context.toString())
@@ -56,8 +66,83 @@ public class KNNwithClassImbalance extends kNN {
         return major;
     }
 
+    //DA RIVEDERE
+    private void purify(){
+        int major = majorClass();
+        ArrayList<Integer> impurity = new ArrayList<>();
+        for(int i = 0; i< window.numInstances();i++){
+            if(window.get(i).classValue() == major){
+                Instances neighbours = findKNNeighbours(window.get(i),Kpurity.getValue());
+                if(neighbours == null)
+                    return;
+                int cntMajor = 0;
+                for(int n = 0; n < neighbours.numInstances(); n++) {
+                    if( (int)neighbours.get(n).classValue() == major)
+                        cntMajor++;
+
+                }
+                if(cntMajor < (Kpurity.getValue()+1)/sizes.size())
+                    impurity.add(i);
+
+            }else{
+
+                Instances neighbours = findKNNeighbours(window.get(i),Kpurity.getValue());
+                if(neighbours == null)
+                    return;
+                int cntMajor = 0;
+                for(int n = 0; n < neighbours.numInstances(); n++) {
+                    if( (int)neighbours.get(n).classValue() == major)
+                        cntMajor++;
+
+                }
+                if(cntMajor < (Kpurity.getValue()+1)/sizes.size()){
+                    for(int n = 0; n < neighbours.numInstances(); n++)
+                        if(neighbours.get(n).classValue() == major)
+                            impurity.add(window.indexOf(neighbours.instance(n)));
+                }
+
+            }
+        }
+
+        if(impurity.size() > 0) {
+            System.out.println("number of classes: " + sizes.size());
+            System.out.println("major: " + major);
+            System.out.println(impurity.size());
+            for (int s : sizes)
+                System.out.println("size " + s);
+        }
+        Collections.sort(impurity);
+        for(int m = impurity.size()-1;m>=0;m--)
+            window.delete(impurity.get(m));
+        sizes.set(major, sizes.get(major) - impurity.size());
+
+
+
+    }
+
+    public double imbalance(){
+        if(sizes.size() < 2 || getTotalInstances() < Kpurity.getValue())
+            return 0;
+
+        double balance = 1d;
+
+
+
+        for(int s = 0; s<sizes.size();s++)
+            balance *= ((double) sizes.get(s))/((double) window.size());
+
+        return 1d - (balance/(double)(sizes.size() * sizes.size()));
+    }
+
     @Override
     public void trainOnInstanceImpl(Instance inst) {
+//        double imb = imbalance();
+//        if( this.purifyFreq.getValue() > r.nextDouble() && imb > 0.0) {
+//            System.out.println("size0: " + sizes.get(0) + " size1: " + sizes.get(1) + " imbalance= " + imb);
+//            purify();
+//        }
+
+
         int c = (int)inst.classValue();
         if (c > C)
             C = c;
@@ -69,6 +154,8 @@ public class KNNwithClassImbalance extends kNN {
         }
 
         if (this.limitOption.getValue() <= this.getTotalInstances()) {
+            //this.sizes.set((int)window.get(window.numInstances()-1).classValue(), sizes.get((int)window.get(window.numInstances()-1).classValue()) - 1);
+            //window.delete(window.numInstances()-1);
             int major = majorClass();
 
             for(int i= window.numInstances()-1;i>=0;i--){
@@ -85,10 +172,7 @@ public class KNNwithClassImbalance extends kNN {
         this.window.add(inst);
     }
 
-
-    @Override
-    public double[] getVotesForInstance(Instance inst) {
-        double v[] = new double[C+1];
+    private Instances findKNNeighbours(Instance inst,int kvalue){
         try {
             NearestNeighbourSearch search;
             if (this.nearestNeighbourSearchOption.getChosenIndex()== 0) {
@@ -98,19 +182,33 @@ public class KNNwithClassImbalance extends kNN {
                 search.setInstances(this.window);
             }
             if (this.window.numInstances()>0) {
-                Instances neighbours = search.kNearestNeighbours(inst,Math.min(kOption.getValue(),this.window.numInstances()));
-                for(int i = 0; i < neighbours.numInstances(); i++) {
-                    int c = (int)neighbours.instance(i).classValue();
-                    v[c] += getClassWeight(c);
-                }
+                return search.kNearestNeighbours(inst,Math.min(kvalue,this.window.numInstances()));
             }
         } catch(Exception e) {
             //System.err.println("Error: kNN search failed.");
             //e.printStackTrace();
             //System.exit(1);
-            return new double[inst.numClasses()];
+            return null;
         }
+        return null;
+    }
+
+
+    @Override
+    public double[] getVotesForInstance(Instance inst) {
+        double v[] = new double[C+1];
+        Instances neighbours = findKNNeighbours(inst, kOption.getValue());
+        if(neighbours == null)
+            return new double[inst.numClasses()];
+
+        for(int i = 0; i < neighbours.numInstances(); i++) {
+            int c = (int)neighbours.instance(i).classValue();
+            v[c] += getClassWeight(c);
+        }
+
         return v;
+
+
     }
 
 }
--- moa/src/main/java/moa/classifiers/meta/imbalace/ARFwithClassImbalance.java
+++ moa/src/main/java/moa/classifiers/meta/imbalace/ARFwithClassImbalance.java
@@ -6,6 +6,7 @@ import moa.classifiers.meta.AdaptiveRandomForest;
 import moa.core.DoubleVector;
 import moa.core.InstanceExample;
 import moa.core.MiscUtils;
+import weka.classifiers.functions.neural.NeuralConnection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,7 +14,6 @@ import java.util.Collection;
 import java.util.concurrent.ExecutorService;
 
 public class ARFwithClassImbalance extends AdaptiveRandomForest {
-
 
     public FloatOption theta = new FloatOption("theta", 't',
             "The time decay factor for class size.", 0.9, 0, 1);
@@ -24,6 +24,8 @@ public class ARFwithClassImbalance extends AdaptiveRandomForest {
 
     @Override
     public void trainOnInstanceImpl(Instance instance) {
+        super.disableWeightedVote.setValue(false);
+        super.disableDriftDetectionOption.setValue(true);
         updateClassSize(instance);
         super.lambdaOption.setValue(calculatePoissonLambda(instance));
         super.trainOnInstanceImpl(instance);
@@ -34,6 +36,8 @@ public class ARFwithClassImbalance extends AdaptiveRandomForest {
 
 
         int majClass = getMajorityClass();
+        if ((int) inst.classValue() == majClass)
+            return 1d;
         //if((int) inst.classValue() == majClass)
             //return 1d;
         return 1d/classSize[(int) inst.classValue()];
