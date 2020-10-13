16ea5fbb7ddb01706370c0af38a8a9cbd7533f25
Enrico <enrico.voltan@mail.polimi.it>
2020-10-13 18:52:33 +0200

    train OS-ELM

--- moa/src/main/java/moa/classifiers/meta/ELM/OSelm.java
+++ moa/src/main/java/moa/classifiers/meta/ELM/OSelm.java
@@ -8,6 +8,7 @@ import com.yahoo.labs.samoa.instances.Instances;
 import moa.classifiers.AbstractClassifier;
 import moa.core.Measurement;
 import no.uib.cipr.matrix.DenseMatrix;
+import no.uib.cipr.matrix.DenseVector;
 import no.uib.cipr.matrix.NotConvergedException;
 
 import java.util.ArrayList;
@@ -64,9 +65,33 @@ public class OSelm extends AbstractClassifier {
         return new double[0];
     }
 
+    private static DenseMatrix opposite(DenseMatrix m){
+        DenseMatrix z = new DenseMatrix(m.numRows(),m.numColumns());
+        z.zero();
+        for(int i = 0; i<z.numRows(); i++)
+            for(int j = 0; j<z.numColumns(); j++)
+                z.add(i,j,-1);
+        return mult(m,z);
+    }
 
-    private DenseMatrix transpose(DenseMatrix m){
+
+    private static DenseMatrix transpose(DenseMatrix m){
         return (DenseMatrix) m.transpose(new DenseMatrix(m.numColumns(),m.numRows()));
+    }
+
+    private static  DenseMatrix mult(DenseMatrix A, DenseMatrix B){
+        return (DenseMatrix) A.mult(B,new DenseMatrix(A.numRows(),B.numColumns()));
+    }
+
+    public static DenseMatrix randomMatrix(int rows, int columns, int seed) {
+        Random x = new Random(seed);
+        DenseMatrix source = new DenseMatrix(rows, columns);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                source.set(i, j, x.nextDouble());
+            }
+        }
+        return source;
     }
 
     private void initialization() throws NotConvergedException {
@@ -136,21 +161,56 @@ public class OSelm extends AbstractClassifier {
 
     @Override
     public void trainOnInstanceImpl(Instance inst) {
-        //P = P-PH'(I+HPH')ˆ(-1)HP
-        //outputWeights B = B + PH'(Y-HB)
-    }
+        //H chunk k+1
+        //P k+1 = P-PH'(I+HPH')ˆ(-1)HP
+        //outputWeights k+1 B = B + PH'(Y-HB)
+
+        //Sherman–Morrison formula
+        DenseMatrix transP = new DenseMatrix(1,init_inst.numAttributes());
+        for (int i = 0; i < init_inst.numAttributes(); i++)
+            transP.set(0, i, inst.value(i));
+
+        DenseMatrix tempH = new DenseMatrix(init_inst.numClasses(),1);
+        InputWeight.mult(transpose(transP), tempH);
+
+        DenseMatrix BiasMatrix = new DenseMatrix(numberofHiddenNeurons.getValue(),1);
 
 
-    public static DenseMatrix randomMatrix(int rows, int columns, int seed) {
-        Random x = new Random(seed);
-        DenseMatrix source = new DenseMatrix(rows, columns);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                source.set(i, j, x.nextDouble());
-            }
+        for (int i = 0; i < numberofHiddenNeurons.getValue(); i++) {
+            BiasMatrix.set(i, 0, BiasofHiddenNeurons.get(i, 0));
         }
-        return source;
+
+        tempH.add(BiasMatrix);
+
+        DenseMatrix h = new DenseMatrix(1,init_inst.numAttributes());
+
+        for (int j = 0; j < numberofHiddenNeurons.getValue(); j++) {
+            double temp = tempH.get(j, 0);
+            temp = Math.sin(temp);
+            h.set(0, j, temp);
+        }
+
+        DenseMatrix numerator = mult(mult(mult(P,h),transpose(h)),P);
+        DenseMatrix denominator = mult(mult(transpose(h),P),h);
+        for(int i = 0; i<denominator.numRows(); i++)
+            for(int j = 0; j<denominator.numColumns(); j++)
+                denominator.add(i,j,1);
+
+        P.add(-1, mult(numerator,(new Inverse(denominator)).getInverse())); //P update
+
+        DenseMatrix t = new DenseMatrix(1,init_inst.numClasses());
+        t.zero();
+        t.add(0,(int)inst.classValue(),1);
+
+        DenseMatrix adj = mult(transpose(h),outputWeights);
+        adj = opposite(adj);
+        adj.add(transpose(t));
+
+        outputWeights.add(mult(P,mult(h,adj)));
+
     }
+
+
 
     @Override
     public void resetLearningImpl() {
